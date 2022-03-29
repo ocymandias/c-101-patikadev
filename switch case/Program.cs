3ca769abce8610bea0dccc15b10b7f40a617f505
ocymandias <ogulcan981kuyumcuoglu@gmail.com>
2022-03-29 17:24:42 +0300

    switch case
    switch case

---
+++ switch case/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+              case 1:
+                Console.WriteLine("ocak ayındasın");
+                break;
+              case 2:
+                Console.WriteLine("şubat ayındasın");
+                break;
+              case 4:
+                Console.WriteLine("nisan ayındasın");
+                break;
+              case 3:
+                Console.WriteLine("mart ayındasın");
+                break;
+
+                default:
+                Console.WriteLine("yanlış veri girişi");
+                break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                   Console.WriteLine("kış ayındasın");
+                   break;
+                case 3:
+                case 4:
+                case 5:
+                   Console.WriteLine("ilkbahar ayındasın");
+                   break;
+                case 6:
+                case 7:
+                case 8:
+                   Console.WriteLine("yaz ayındasın");
+                   break;
+                case 9:
+                case 10:
+                case 11:
+                   Console.WriteLine("sonbahar ayındasın");
+                   break;
+                
+                default:
+                break;
+            }
+        }
+    }
+}
