fbbad048622f9481f3dbf178f5314fc185fe3208
ocymandias <ogulcan981kuyumcuoglu@gmail.com>
2022-03-29 16:19:45 +0300

    if else
    if else

---
+++ if else/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if(time>=6 && time <11)
+              Console.WriteLine("gunaydin");
+            else if(time<= 18)
+              Console.WriteLine("iyi günler");
+            else
+              Console.WriteLine("iyi geceler");
+
+            string sonuc = time<=18 ? "iyi günler" : "iyi geceler";
+            sonuc = time>=6 && time<11 ? "günaydın" : time<=18 ? "iyi günler" : "iyi geceler";
+            Console.WriteLine(sonuc);
+
+
+        }
+    }
+}
