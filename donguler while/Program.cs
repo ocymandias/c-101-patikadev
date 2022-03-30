9e1fcc8964669f3de920961b345597c4899c4485
ocymandias <ogulcan981kuyumcuoglu@gmail.com>
2022-03-30 22:01:37 +0300

    donguler while
    donguler while

---
+++ donguler while/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("sayi giriniz:");
+             int sayi = int.Parse(Console.ReadLine());
+             int sayac = 1;
+             int toplam = 0;
+            while (sayac<=sayi)
+            {
+                 toplam += sayac;
+                 sayac ++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            char character = 'a';
+            while (character < 'z')
+             {
+                Console.Write(character);
+                character ++;
+             }
+             
+             string[] arabalar = {"bmw","ford","toyota","nissan"};
+             foreach (var araba in arabalar)
+             {
+                 Console.WriteLine(araba);
+             }
+        }
+    }
+}
