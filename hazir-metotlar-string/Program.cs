833d69a34643643df26270117efa77e9ee293f55
ocymandias <ogulcan981kuyumcuoglu@gmail.com>
2022-04-02 12:06:20 +0300

    hazir-metotlar-string
    hazir-metotlar-string

---
+++ hazir-metotlar-string/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string degisken = "Dersimiz CSharp, hosgeldiniz";
+            string degisken2 = "CSharp";
+
+            Console.WriteLine(degisken.Length);
+            Console.WriteLine(degisken.ToUpper());
+            Console.WriteLine(degisken.ToLower());
+            Console.WriteLine(String.Concat(degisken," merhaba"));
+            Console.WriteLine(degisken.CompareTo(degisken2));
+            Console.WriteLine(String.Compare(degisken,degisken2,true));
+            Console.WriteLine(String.Compare(degisken,degisken2,false));
+            Console.WriteLine(degisken.Contains(degisken2));
+            Console.WriteLine(degisken.EndsWith("hosgeldiniz"));
+            Console.WriteLine(degisken.StartsWith("merhaba"));
+            Console.WriteLine(degisken.IndexOf("CSharp"));
+            Console.WriteLine(degisken.IndexOf("aaaaa"));
+            Console.WriteLine(degisken.LastIndexOf("i"));
+            Console.WriteLine(degisken.Insert(0,"merhaba "));
+            Console.WriteLine(degisken + degisken2.PadLeft(30));
+            Console.WriteLine(degisken + degisken2.PadLeft(30,'*'));
+            Console.WriteLine(degisken.PadRight(50) + degisken2);
+            Console.WriteLine(degisken.PadRight(50,'-') + degisken2); 
+            Console.WriteLine(degisken.Remove(10));
+            Console.WriteLine(degisken.Remove(5,3));
+            Console.WriteLine(degisken.Remove(0,1));
+            Console.WriteLine(degisken.Replace("CSharp","C#"));
+            Console.WriteLine(degisken.Replace(" ","*"));
+            Console.WriteLine(degisken.Split(' ')[1]);
+            Console.WriteLine(degisken.Substring(4));
+            Console.WriteLine(degisken.Substring(4,6));
+            
+
+        }
+    }
+}
---
+++ odev1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. soru:lütfen pozitif bir sayı giriniz");
+            int n = int.Parse(Console.ReadLine());
+            int[] sayidizisi = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("lütfen {0}. sayıyı gir:", i+1);
+                sayidizisi[i] = int.Parse(Console.ReadLine());
+            }
+        for (int i = 0; i < n; i++)
+            {
+            if(sayidizisi[i]%2 ==0)
+              {
+               Console.WriteLine(sayidizisi[i] + ":çift sayıdır");
+              } 
+            }
+        
+        Console.WriteLine("2. soru:lütfen pozitif iki sayı giriniz");
+        int k = int.Parse(Console.ReadLine());
+        int j = int.Parse(Console.ReadLine());
+        int[] sayidizisi2 = new int[k];
+        for (int i = 0; i < k; i++)
+            {
+                Console.Write("lütfen {0}. sayıyı gir:", i+1);
+                sayidizisi2[i] = int.Parse(Console.ReadLine());
+            }
+        for(int i=0; i<k;i++)
+           {
+               if(sayidizisi2[i]%j==0)
+               Console.WriteLine(sayidizisi2[i] +" sayısı " + j+ " sayısına tam bölünür");
+           }
+        Console.WriteLine("3. soru:lütfen pozitif bir sayı giriniz");
+            int z = int.Parse(Console.ReadLine());
+            string[] kelimedizisi = new string[z];
+            for (int i = 0; i < z; i++)
+            {
+                Console.Write("lütfen {0}. kelimeyi gir:", i+1);
+                kelimedizisi[i] = Console.ReadLine();
+            }
+                Array.Reverse(kelimedizisi);
+            for (int i = 0; i < z; i++)
+            {
+                Console.WriteLine(kelimedizisi[i]);
+            }
+        Console.WriteLine("4. soru:lütfen bir cümle yazınız");
+            string kelimedizisi2 = Convert.ToString(Console.ReadLine());
+            string[] kelime = kelimedizisi2.Split(" ");
+            Console.WriteLine("cümlede {0} kadar kelime vardır" , kelime.Length);
+            Console.WriteLine("cümlede {0} kadar harf vardır" , kelimedizisi2.Length);
+        } 
+    }
+}
