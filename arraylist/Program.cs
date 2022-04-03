2f967c32214d499ae7e7be1e1f5f8e69b78787ab
ocymandias <ogulcan981kuyumcuoglu@gmail.com>
2022-04-03 10:51:53 +0300

    arraylist
    arraylist

---
+++ arraylist/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           ArrayList liste = new ArrayList();
+        //    liste.Add("ayşe");
+        //    liste.Add(2);
+        //    liste.Add(true);
+        //    liste.Add('a');
+        //    Console.WriteLine(liste[1]);
+        //    foreach (var item in liste)
+        //      Console.WriteLine(item);
+           
+        //    string[] renkler = {"kırmızı","sarı","yeşil"};
+           List<int> sayılar = new List<int>(){1,8,3,7,9,92,5};
+        //    liste.AddRange(renkler);
+           liste.AddRange(sayılar);
+           foreach (var item in liste)
+             Console.WriteLine(item);
+
+
+           liste.Sort();
+            foreach (var item in liste)
+             Console.WriteLine(item);
+
+
+           Console.WriteLine(liste.BinarySearch(9));
+
+
+           liste.Reverse();
+           foreach (var item in liste)
+              Console.WriteLine(item);
+
+
+           liste.Clear();
+
+            foreach (var item in liste)
+             Console.WriteLine(item);
+
+        }
+    }
+}
