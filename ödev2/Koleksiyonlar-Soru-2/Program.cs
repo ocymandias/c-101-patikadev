f66064552a03895e5e4e17dd48934a740b163dfb
ocymandias <ogulcan981kuyumcuoglu@gmail.com>
2022-04-03 16:43:23 +0300

    ödev2
    ödev2

--- ödev2/Koleksiyonlar-Soru-2/Program.cs
+++ ödev2/Koleksiyonlar-Soru-2/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen 20 adet tam sayı giriniz.");
+            int[] sayilar = new int[20];
+            int[] kucukler = new int[3];
+            int[] buyukler = new int[3];
+            for (int i = 1; i < 21; i++)
+            {
+                Console.Write("{0}. sayıyı giriniz: ", i);
+                sayilar[i-1] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(sayilar);
+            kucukler[0] = sayilar [0];
+            kucukler[1] = sayilar [1];
+            kucukler[2] = sayilar [2];
+            foreach (var item in kucukler)
+            {
+                Console.WriteLine(item);
+            }
+            Array.Reverse(sayilar);
+            buyukler[0] = sayilar [0];
+            buyukler[1] = sayilar [1];
+            buyukler[2] = sayilar [2];
+            foreach (var item in buyukler)
+            {
+                Console.Write(item);
+            }
+            
+
+            int sayac = 1;
+            int toplam = 0;
+            for(int i=0; i<kucukler.Length ; i++)
+            {
+                 toplam = toplam + kucukler[i];
+                 sayac ++;
+            }
+            Console.WriteLine("en küçük 3 sayının ortalaması:" + toplam/kucukler.Length);
+            int sayac2 = 1;
+            int toplam2 = 0;
+            for(int i=0; i<buyukler.Length ; i++)
+            {
+                 toplam2 = toplam2 + buyukler[i];
+                 sayac2 ++;
+            }
+            Console.WriteLine("en büyük 3 sayının ortalaması:" + toplam2/buyukler.Length);
+            Console.WriteLine("ortalamaları toplamı:" + (toplam/kucukler.Length)+(toplam2/buyukler.Length));
+
+
+        }
+
+    }
+}
---
+++ ödev2/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lütfen bir cümle giriniz");
+            string cumle=Console.ReadLine();
+            char[] sesliler ={'a','e','ı','i','u','ü','o','ö'};
+            string harfler = "";
+
+            foreach (var item in cumle)
+             if(item == 'a' || item == 'e' || item == 'ı' || item == 'i' || item == 'o' || item == 'ö' || item == 'u' || item == 'ü')
+                harfler += item;
+
+            foreach (var item in harfler)
+            {
+                Console.WriteLine(item);
+            }    
+        }
+    }
+}
